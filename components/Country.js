90629bfee12c62fb570647af6c4bc7239f9f4740
unknown <emranulfarhad@gmail.com>
2022-05-29 15:55:33 +0600

    text input image include

--- components/Country.js
+++ components/Country.js
@@ -1,4 +1,4 @@
-import { View, Text ,ScrollView } from 'react-native'
+import { View, Text ,ScrollView , StyleSheet,  TextInput  } from 'react-native'
 import React ,{ useState , useEffect } from 'react'
 import Countrys from './Countrys'
 
@@ -9,21 +9,46 @@ export default function Country() {
 
 
     const [countrys, setCountry] = useState([])
+    const [search , setSeacrh] = useState([])
+
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
-        .then(data => setCountry(data))
+        .then(data => {
+            setSeacrh(data)
+            setCountry(data)})
     },[] )
+
+    const byname = text => {
+        const bb = countrys.filter( names => names.name.common.includes(text))
+        setSeacrh(bb)
+    }
 
   return (
     <View>
       <Text>Country</Text>
-      <Text> total {countrys.length} </Text>
+      <Text> total {search.length} </Text>
+
+      <TextInput
+      style={styles.input}
+      placeholder = "type country Name"
+      onChangeText={byname}
+      ></TextInput>
+
        <ScrollView>
            {
-               countrys.map(country => <Countrys  key={country.id} country={country} ></Countrys> )
+               search.map(country => <Countrys  key={country.id} country={country} ></Countrys> )
            }
        </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+   input : {
+       height : 40,
+       margin : 12,
+       borderWidth : 1,
+       padding : 10,
+   }
+})
--- components/Countrys.js
+++ components/Countrys.js
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text , Image } from 'react-native'
 import React from 'react'
 
 
 export default function Countrys({country}) {
   return (
-    <View>
-      <Text>   {country.name.common} </Text>
+    <View>  
+     <Text> Country : {country.name.common} </Text>
+      <Image
+      source={{
+        uri : country.flags.png
+      }}
+      style={{width: 200, height: 200}}
+      ></Image>
     </View>
   )
 }

85a478b39092c721c16c5f361d00376e1ad39707
unknown <emranulfarhad@gmail.com>
2022-05-29 12:25:44 +0600

    react components add

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Country from './components/Country';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Country></Country>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ components/Country.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+
+
+export default function Country() {
+
+
+  return (
+    <View>
+    
+      <Text> Hello world !! first rect apps </Text>
+    </View>
+  )
+}
